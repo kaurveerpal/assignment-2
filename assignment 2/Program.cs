1bc9daa07654385493f9bda470428b008293f3e9
visualstudio <visualstudio@122-PC37>
2019-03-06 11:28:07 -0500

    lab 1

--- assignment 2/Program.cs
+++ assignment 2/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// student name veerpal  kaur, dikshat chawala
+//student id c0730133, c0732121
+// assignment 2
+// date march6,2019
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
 
-namespace assignment_2
+    }
+}
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
